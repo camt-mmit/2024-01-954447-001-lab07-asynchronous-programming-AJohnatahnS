28b466db78522c8936f0710de6bdc94ed27cfa1a
AJohnatahnS <95701398+AJohnatahnS@users.noreply.github.com>
2025-03-27 18:57:52 +0700

    ::CC

---
+++ src/app/profile/pages/profile-page/profile-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-profile-page',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './profile-page.component.html',
+  styleUrl: './profile-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProfilePageComponent {}
